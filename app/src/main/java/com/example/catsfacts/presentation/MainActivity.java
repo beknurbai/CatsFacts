d0cfd737b54e60e930c21d7d11638ce4b511ad6f
beknurbai <beknurbaihan@gmail.com>
2020-08-13 22:54:23 +0600

    WorkProject2.0

--- app/src/main/java/com/example/catsfacts/presentation/MainActivity.java
+++ app/src/main/java/com/example/catsfacts/presentation/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.catsfacts.R;
 import com.example.catsfacts.presentation.adapters.SlideFragmentAdapter;
@@ -23,7 +24,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         getSupportFragmentManager().beginTransaction().replace(R.id.container_fr, new ViewPagerFragment()).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        String currentFragmentName = getSupportFragmentManager().findFragmentById(R.id.container_fr).getClass().getSimpleName();
+        Log.d("check", currentFragmentName);
+        if (currentFragmentName.equals("FactsFragment")) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_fr, new ViewPagerFragment()).commit();
+        } else {
+            super.onBackPressed();
+
+        }
+
     }
 }
